eb33ac468fc80b5a98420049e646b5eb313a5690
Benedikt Schnatterbeck <benediktms@hey.com>
2022-12-02 13:55:49 +0000

    Add part 2 of day 1

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -4,24 +4,23 @@ use std::{
     path::Path,
 };
 
-fn main() -> std::io::Result<()> {
+fn main() -> anyhow::Result<()> {
     let path = Path::new("input/day1/input1.txt");
     let file = File::open(path)?;
 
     let buf_reader = BufReader::new(file);
-    let lines = buf_reader.lines();
 
     let mut calories = vec![0];
 
-    for line in lines {
+    for line in buf_reader.lines() {
         let line = line?;
 
-        match line.parse::<i32>() {
-            Ok(val) => {
-                let prev = calories.last_mut().unwrap();
-                *prev += val;
-            }
-            Err(_) => calories.push(0),
+        if line.is_empty() {
+            calories.push(0);
+        } else {
+            let val = line.parse::<i32>()?;
+            let prev = calories.last_mut().unwrap();
+            *prev += val;
         }
     }
 
@@ -29,6 +28,14 @@ fn main() -> std::io::Result<()> {
     println!("largest number: {}", largest);
 
     assert_eq!(*largest, 67633);
+
+    calories.sort_by(|a, b| b.cmp(a));
+
+    assert_eq!(calories[..3], [67633, 66296, 65699]);
+
+    // todo find total of sorted
+    let total = calories.iter().take(3).sum::<i32>();
+    println!("total of largest 3: {}", total);
 
     Ok(())
 }
