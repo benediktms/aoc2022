76a21fcc2234e9a3d795451d307d882346fbd14d
Benedikt Schnatterbeck <benediktms@hey.com>
2022-12-06 11:12:41 +0000

    Add day2 part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -12,6 +12,24 @@ enum Symbol {
     Scissor,
 }
 
+enum Outcome {
+    Win,
+    Draw,
+    Loose,
+}
+
+impl FromStr for Outcome {
+    type Err = ();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "X" => Ok(Outcome::Loose),
+            "Y" => Ok(Outcome::Draw),
+            "Z" => Ok(Outcome::Win),
+            _ => Err(()),
+        }
+    }
+}
+
 impl FromStr for Symbol {
     type Err = ();
     fn from_str(s: &str) -> Result<Symbol, Self::Err> {
@@ -37,6 +55,16 @@ impl ToString for Symbol {
     }
 }
 
+impl ToString for Outcome {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Draw => "Draw".to_string(),
+            Self::Loose => "Loose".to_string(),
+            Self::Win => "Win".to_string(),
+        }
+    }
+}
+
 impl PartialEq for Symbol {
     fn eq(&self, other: &Self) -> bool {
         let a = &self.to_string();
@@ -46,40 +74,78 @@ impl PartialEq for Symbol {
     }
 }
 
-fn play_rps(opponent_symbol: &str, your_symbol: &str) -> i32 {
-    let oppenent_symbol = Symbol::from_str(opponent_symbol).unwrap();
-    let your_symbol = Symbol::from_str(your_symbol).unwrap();
+impl PartialEq for Outcome {
+    fn eq(&self, other: &Self) -> bool {
+        let a = &self.to_string();
+        let b = &other.to_string();
 
+        a == b
+    }
+}
+
+fn determine_symbol(oppenent_symbol: &Symbol, desired_outcome: &Outcome) -> Symbol {
+    match oppenent_symbol {
+        Symbol::Rock => {
+            if desired_outcome.eq(&Outcome::Win) {
+                Symbol::Paper
+            } else if desired_outcome.eq(&Outcome::Draw) {
+                Symbol::Rock
+            } else {
+                Symbol::Scissor
+            }
+        }
+        Symbol::Paper => {
+            if desired_outcome.eq(&Outcome::Win) {
+                Symbol::Scissor
+            } else if desired_outcome.eq(&Outcome::Draw) {
+                Symbol::Paper
+            } else {
+                Symbol::Rock
+            }
+        }
+        Symbol::Scissor => {
+            if desired_outcome.eq(&Outcome::Win) {
+                Symbol::Rock
+            } else if desired_outcome.eq(&Outcome::Draw) {
+                Symbol::Scissor
+            } else {
+                Symbol::Paper
+            }
+        }
+    }
+}
+
+fn play_rps(opponent_symbol: &Symbol, your_symbol: &Symbol) -> i32 {
     let mut base: i32 = 0;
 
     match your_symbol {
         Symbol::Paper => {
             base += 2;
-            if oppenent_symbol.eq(&Symbol::Scissor) {
+            if opponent_symbol.eq(&Symbol::Scissor) {
                 base += 0;
-            } else if oppenent_symbol.eq(&Symbol::Rock) {
+            } else if opponent_symbol.eq(&Symbol::Rock) {
                 base += 6
-            } else if oppenent_symbol.eq(&Symbol::Paper) {
+            } else if opponent_symbol.eq(&Symbol::Paper) {
                 base += 3
             }
         }
         Symbol::Rock => {
             base += 1;
-            if oppenent_symbol.eq(&Symbol::Scissor) {
+            if opponent_symbol.eq(&Symbol::Scissor) {
                 base += 6;
-            } else if oppenent_symbol.eq(&Symbol::Rock) {
+            } else if opponent_symbol.eq(&Symbol::Rock) {
                 base += 3
-            } else if oppenent_symbol.eq(&Symbol::Paper) {
+            } else if opponent_symbol.eq(&Symbol::Paper) {
                 base += 0
             }
         }
         Symbol::Scissor => {
             base += 3;
-            if oppenent_symbol.eq(&Symbol::Scissor) {
+            if opponent_symbol.eq(&Symbol::Scissor) {
                 base += 3;
-            } else if oppenent_symbol.eq(&Symbol::Rock) {
+            } else if opponent_symbol.eq(&Symbol::Rock) {
                 base += 0
-            } else if oppenent_symbol.eq(&Symbol::Paper) {
+            } else if opponent_symbol.eq(&Symbol::Paper) {
                 base += 6
             }
         }
@@ -96,19 +162,24 @@ fn main() -> anyhow::Result<()> {
     let lines = buf_reader.lines();
 
     let mut score = 0;
+    let mut determined_score = 0;
 
     for line in lines {
         let line = line?;
         let split: Vec<&str> = line.split_whitespace().collect();
         let split: (&str, &str) = (split[0], split[1]);
 
-        let oppenent = split.0;
-        let you = split.1;
+        let opponent = Symbol::from_str(split.0).unwrap();
+        let you = Symbol::from_str(split.1).unwrap();
+        let determined_symbol = determine_symbol(&opponent, &Outcome::from_str(split.1).unwrap());
 
-        score += play_rps(oppenent, you);
+        determined_score += play_rps(&opponent, &determined_symbol);
+        score += play_rps(&opponent, &you);
     }
 
+    assert_eq!(score, 11906);
     println!("score: {}", score);
+    println!("determined_score: {}", determined_score);
 
     Ok(())
 }
