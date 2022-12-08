95cab2a8a15a653c57673a3f7d58d3682820764f
Benedikt Schnatterbeck <benediktms@hey.com>
2022-12-08 10:18:34 +0000

    Initial attempt at day3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,11 +1,34 @@
-use itertools::{Itertools, TupleWindows};
+use itertools::Itertools;
 use std::{
     fs::File,
     io::{BufRead, BufReader},
     path::Path,
 };
 
-fn get_item_priorities(backpack: (&str, &str)) -> i32 {
+fn check_against_alphabet(
+    item: &char,
+    comparitor: &Vec<char>,
+    additional_offset: Option<i32>,
+) -> i32 {
+    let mut sum = 0;
+    let index = comparitor.iter().position(|char| char == item);
+    let offset = match additional_offset {
+        Some(num) => num as i32,
+        None => 0,
+    };
+
+    match index {
+        Some(value) => {
+            let priority = (value as i32) + 1 + offset;
+            sum += priority as i32
+        }
+        None => sum += 0,
+    };
+
+    sum
+}
+
+fn get_alphabets() -> (Vec<char>, Vec<char>) {
     let lowercase_range = b'a'..=b'z';
     let uppercase_range = b'A'..=b'Z';
 
@@ -16,6 +39,12 @@ fn get_item_priorities(backpack: (&str, &str)) -> i32 {
     let uppercase_vec = uppercase_range
         .map(|i| (i as char).to_ascii_uppercase())
         .collect_vec();
+
+    (lowercase_vec, uppercase_vec)
+}
+
+fn get_item_priorities(backpack: (&str, &str)) -> i32 {
+    let (lowercase_vec, uppercase_vec) = get_alphabets();
 
     let first_compartment = backpack.0.chars().collect_vec();
     let second_compartment = backpack.1.chars().collect_vec();
@@ -28,45 +57,8 @@ fn get_item_priorities(backpack: (&str, &str)) -> i32 {
             is_duplicate = outer_item == inner_item;
 
             if is_duplicate {
-                let index = lowercase_vec.iter().position(|char| char == inner_item);
-                match index {
-                    Some(value) => {
-                        // account for 0th index
-                        let priority = value + 1;
-                        sum += priority as i32
-                    }
-                    None => {
-                        let index = lowercase_vec.iter().position(|char| char == inner_item);
-                        match index {
-                            Some(value) => {
-                                // account for 0th index
-                                let priority = (value as i32) + 1;
-                                sum += priority
-                            }
-                            None => sum += 0,
-                        };
-                    }
-                };
-
-                let index = uppercase_vec.iter().position(|char| char == inner_item);
-                match index {
-                    Some(value) => {
-                        // account for the first 26 characters from the lowercase verion and 0th index
-                        let priority = value + 1 + 26;
-                        sum += priority as i32
-                    }
-                    None => {
-                        let index = uppercase_vec.iter().position(|char| char == inner_item);
-                        match index {
-                            Some(value) => {
-                                // account for the first 26 characters from the lowercase verion and 0th index
-                                let priority = (value as i32) + 26 + 1;
-                                sum += priority
-                            }
-                            None => sum += 0,
-                        };
-                    }
-                };
+                sum += check_against_alphabet(inner_item, &lowercase_vec, None);
+                sum += check_against_alphabet(inner_item, &uppercase_vec, Some(26));
 
                 break;
             }
@@ -80,6 +72,54 @@ fn get_item_priorities(backpack: (&str, &str)) -> i32 {
     sum
 }
 
+// probably this can be refactored in a way so that everything can be done in `get_item_priorities`
+fn caculate_total_of_badges_in_groups(lines: Vec<String>) -> i32 {
+    let (lowercase_vec, uppercase_vec) = get_alphabets();
+
+    let mut i = 0;
+    let mut score = 0;
+
+    while i < lines.len() {
+        let row_one = &lines[i].chars().collect_vec();
+        i += 1;
+
+        let row_two = &lines[i].chars().collect_vec();
+        i += 1;
+
+        let row_three = &lines[i].chars().collect_vec();
+        i += 1;
+
+        let mut is_duplicate = false;
+
+        for item_one in row_one {
+            for item_two in row_two {
+                for item_three in row_three {
+                    if item_three == item_one && item_three == item_two {
+                        is_duplicate = true;
+                    }
+
+                    if is_duplicate {
+                        score += check_against_alphabet(item_three, &lowercase_vec, None);
+                        score += check_against_alphabet(item_three, &uppercase_vec, None);
+
+                        break;
+                    }
+                }
+
+                if is_duplicate {
+                    break;
+                }
+            }
+
+            if is_duplicate {
+                break;
+            }
+        }
+    }
+
+    score
+}
+
 fn main() -> anyhow::Result<()> {
     let path = Path::new("input/day3/input1.txt");
     let file = File::open(path)?;
@@ -87,42 +127,27 @@ fn main() -> anyhow::Result<()> {
     let buf_reader = BufReader::new(file);
     let mut lines = buf_reader.lines();
     let mut sum = 0;
+    let mut string_lines: Vec<String> = Vec::new();
 
     for line in lines.by_ref() {
         let line = line?;
+
+        // empty slice array only used to get the length
         let split = line.split(|i: char| i.is_ascii_alphabetic()).collect_vec();
 
         let backpack_compartments = line.split_at((split.len() - 1) / 2);
 
         sum += get_item_priorities(backpack_compartments);
+        string_lines.push(line);
     }
 
-    // select 3 rows at a time
-    let mapped = lines
-        .map(|line| match line {
-            Ok(line) => line,
-            Err(err) => panic!("Invalid string: {}", err),
-        })
-        .collect_vec();
-
-    // let chunks: TupleWindows<std::slice::Iter<String>, (&String, &String, &String)> =
-    let chunks = mapped.iter().chunks(3);
-
-    for chunk in chunks.into_iter() {
-        println!("{:?}", chunk.collect_vec())
-    }
-
-    // for chunk in chunks {
-    //     for group in chunk {
-    //         println!("{}", group)
-    //     }
-    // }
-
-    // find the one letter (the badge) (upper or lower) that appears in all three lines
-    // keep track of the sum of the priority the badges in a seperate variable
+    let sum_of_badges = caculate_total_of_badges_in_groups(string_lines);
 
     println!("sum: {}", sum);
     assert_eq!(8123, sum);
+
+    // not 1242
+    println!("sum of badges: {}", sum_of_badges);
 
     Ok(())
 }
@@ -135,7 +160,7 @@ mod tests {
         path::Path,
     };
 
-    use crate::get_item_priorities;
+    use crate::{caculate_total_of_badges_in_groups, get_item_priorities};
 
     #[test]
     fn should_cacluate_the_correct_value_from_test_input() -> anyhow::Result<()> {
@@ -144,6 +169,8 @@ mod tests {
 
         let buf_reader = BufReader::new(file);
         let lines = buf_reader.lines();
+        let mut string_lines: Vec<String> = Vec::new();
+
         let mut sum = 0;
 
         for line in lines {
@@ -153,10 +180,16 @@ mod tests {
             let backpack = line.split_at((split.len() - 1) / 2);
 
             sum += get_item_priorities(backpack);
+            string_lines.push(line);
         }
 
-        println!("score 1: {}", sum);
+        let sum_of_badges = caculate_total_of_badges_in_groups(string_lines);
+
+        println!("sum: {}", sum);
+        println!("sum of badges: {}", sum_of_badges);
+
         assert_eq!(sum, 157);
+        assert_eq!(sum_of_badges, 70);
 
         Ok(())
     }
